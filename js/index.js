3be723ff3ff0310da786984004f5ded4888df9bb
Kaveh <kavehfrounchi@gmail.com>
2021-11-19 01:59:38 +0330

    Completed the about section of my website. Added Bootstrap Cards for the projects section. Completed the first project's card.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    
+    // $('.card').on('mouseenter', function() {
+    //     $('.card').css("background-color", "green");
+    // })
+    
+    // $('.card').on('mouseleave', function() {
+    //     $('.card').css("background-color", "white");
+    // })
+})
