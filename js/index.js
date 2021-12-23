8c716cd0050ce3ecaf065c8f2db8ae62ee991800
Kaveh <kavehfrounchi@gmail.com>
2021-12-23 20:00:40 +0330

    Updated the pictures view in the card to fit into the modal.

--- js/index.js
+++ js/index.js
@@ -9,15 +9,17 @@ $(function() {
             $(".enlarge").attr("data-toggle", "modal");
             $(".enlarge").attr("data-target", '#largen');
            let img = event.target;
-           let width = img.width * 2;
-           let height = img.height * 2;
+        //    let width = img.width * 2;
+        //    let height = img.height * 2;
+        let width = '100%';
+        let height = '100%';
         //    myHtml = "<div class='modal write' id='largen'>"
         myHtml += "<div class='modal write' id='largen'>"
            myHtml += "<div class='modal-dialog'>"
            myHtml += "<div class='modal-content'>"
            myHtml += "<div class='modal-body write'>"
-           myHtml += "<img src=" + img.src + " width=" + width + " height=" + height + " alt='Magnified image'>"
            myHtml += "<button type='button' class='close' data-dismiss='modal'>&times;</button>"
+           myHtml += "<img src=" + img.src + " width=" + width + " height=" + height + " alt='Magnified image'>"
            myHtml += "</div>"
            myHtml += "</div>"
            myHtml += "</div>"
