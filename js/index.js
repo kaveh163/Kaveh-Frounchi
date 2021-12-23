9cecea7e5a929561969fe2b7e6fff234d7849a26
Kaveh <kavehfrounchi@gmail.com>
2021-12-23 19:27:25 +0330

    Added background pictures to cards and made a scroll bar. Edited index.html. Made index.js for dynamcially creating the cards background pictures.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
 $(function() {
-    
+    let bool = true;
+    let imageWindow;
+    let myHtml;
+    let myHtml2;
+    $('.enlarge').on('click', function(event){
+        if(bool) {
+            // $(this).css("transform", "scale(1.5,1.5)");
+            $(".enlarge").attr("data-toggle", "modal");
+            $(".enlarge").attr("data-target", '#largen');
+           let img = event.target;
+           let width = img.width * 2;
+           let height = img.height * 2;
+        //    myHtml = "<div class='modal write' id='largen'>"
+        myHtml += "<div class='modal write' id='largen'>"
+           myHtml += "<div class='modal-dialog'>"
+           myHtml += "<div class='modal-content'>"
+           myHtml += "<div class='modal-body write'>"
+           myHtml += "<img src=" + img.src + " width=" + width + " height=" + height + " alt='Magnified image'>"
+           myHtml += "<button type='button' class='close' data-dismiss='modal'>&times;</button>"
+           myHtml += "</div>"
+           myHtml += "</div>"
+           myHtml += "</div>"
+           myHtml += "</div>"
+            $('#large').append(myHtml);
+            $("body").on('click', '.close', function() {
+                myHtml = '';
+                $('#large').empty();
+            })
+            // bool = false;
+        } 
+        
+    })
     // $('.card').on('mouseenter', function() {
     //     $('.card').css("background-color", "green");
     // })
